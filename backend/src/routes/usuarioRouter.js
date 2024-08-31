8c5d5f9503f6220494bd7ddf38bf3b8e3071f1d8
yanax27 <luchin.ly50@gmail.com>
2024-08-31 13:29:31 -0400

    feat: added config router usuarios

--- backend/src/routes/usuarioRouter.js
+++ backend/src/routes/usuarioRouter.js
@@ -1,10 +1,14 @@
-const express = require("express");
-const router = express();
+const express = require('express');
+const usuarioController = require('../controllers/usuarioController');
+const authMiddleware = require('../middleware/authMiddleware');
+const router = express.Router();
 
-// Define tus rutas aquí
-router.get('/', (req, res) => {
-  res.send('Hello from usuario');
-});
+router.get('/', authMiddleware, usuarioController.getAllUsuarios);
+router.post('/login', usuarioController.login);
+router.post('/logout', usuarioController.logout);
+router.post('/', usuarioController.createUsuario);
+router.get('/:id', authMiddleware,usuarioController.getUsuariobyId);
+router.patch('/:id', authMiddleware, usuarioController.updateUsuario);
+router.delete('/:id', authMiddleware, usuarioController.deleteUsuario);
+
 module.exports = router;
-
-
